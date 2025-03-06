5753d1fafea99805974a2e6b2f77532e54322b72
Sarina <aryalsarinaa@gmail.com>
2025-03-06 15:09:49 +0545

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,24 @@
- export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678',
-  quamtity: 2,
- },{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
- }];
+ export let cart = JSON.parse(localStorage.getItem('cart'));
+
+ if(!cart){
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678',
+    quamtity: 2,
+   },{
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+   }];
+ }
+ 
+ function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+ }
 
  export function addToCart(productId){
   let matchingItem;
+
     cart.forEach((cartItem) => {
-      if(productId == cartItem.productId){
+      if(productId === cartItem.productId){
         matchingItem = cartItem;
       }
     });
@@ -23,16 +32,19 @@
         quantity: 1
       });
     }
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
 
-  cart.forEach(() => {
+  cart.forEach((cartItem) => {
     if(cartItem.productId !== productId){
       newCart.push(cartItem);
     }
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
